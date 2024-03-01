543c1cec441b0e83fa22400d618f061d39ff897c
mdJasim2023 <jasim127md@gmail.com>
2024-03-01 20:51:43 +0530

    first commit

---
+++ JS Assignment/projectInterview/data.js
@@ -0,0 +1,7 @@
+var data = [{"}image1":"https://res.cloudinary.com/difbmfdoo/image/upload/v1708168010/blob_2_cwi6gi.png",
+"image2":"https://res.cloudinary.com/difbmfdoo/image/upload/v1708168010/Ellipse_2_mo0png.png",
+"image3":"https://res.cloudinary.com/difbmfdoo/image/upload/v1708168009/Ellipse_7_o0aiz4.png",
+"image4":"https://res.cloudinary.com/difbmfdoo/image/upload/v1708168009/Ellipse_4_nki2b8.png",
+"image5":"https://res.cloudinary.com/difbmfdoo/image/upload/v1708168009/Ellipse_3_y3axzy.png",
+"text_title":"Taking care for your Smart Dog !",
+"text_summary":"Human-canine bonding is the relationship between dogs and humans"}]
